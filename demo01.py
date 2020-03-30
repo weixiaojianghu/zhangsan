cfaf9577b5b2f804b67666a4417750ccf08dfcfd
weixiaojianghu <1741982178@qq.com>
2020-03-30 17:37:09 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,22 @@
+"""
+print("你好") #字符串
+print(123)
+print(2.33)
+print(True)
+print(())
+print({})
+print([])
+print("哈哈",123,2.33)
+print("哈哈"+"姐姐")
+print("哈哈"*100)
+name="咳咳"
+print(name)
+
+"""
+# a=float(input("请输入："))
+# b=float(2input("请输入："))
+# print("内容为",a+b)
+  
+a=input("请输入一段话:")
+b=input("请输入第二段话：")
+print("您的话长度为",len(a)+len(b))
